af01aca9ec525f7e1ac413b14a366de942145357
mwojciechowski <maciekwoj4@gmail.com>
2025-10-23 01:08:27 +0200

    add timeouts, add headers

--- nodes/Scrapingfish/Scrapingfish.node.ts
+++ nodes/Scrapingfish/Scrapingfish.node.ts
@@ -13,7 +13,7 @@ export class Scrapingfish implements INodeType {
 		icon: 'file:scrapingfish.svg',
 		group: ['transform'],
 		version: 1,
-		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+		subtitle: '={{$parameter["url"]}}',
 		description: 'Interact with the Scrapingfish API',
 		defaults: {
 			name: 'Scrapingfish',
@@ -56,6 +56,15 @@ export class Scrapingfish implements INodeType {
 							'Rules to extract data from the page. See <a href="https://scrapingfish.com/docs/extract-rules">docs</a>.',
 					},
 					{
+						displayName: 'Headers',
+						name: 'headers',
+						type: 'json',
+						default: '',
+						placeholder: '{"X-Custom-Header": "value"}',
+						description:
+							'HTTP headers to forward to the target URL. See <a href="https://scrapingfish.com/docs/forward-headers">docs</a>.',
+					},
+					{
 						displayName: 'JS Scenario',
 						name: 'jsScenario',
 						type: 'json',
@@ -64,6 +73,27 @@ export class Scrapingfish implements INodeType {
 							'{ "steps": [{ "wait": 1000 }, { "click_and_wait_for_navigation": "p > a" }]}',
 						description:
 							'JS instructions to execute on a page. See <a href="https://scrapingfish.com/docs/js-scenario">docs</a>.',
+					},
+					{
+						displayName: 'Render JS Timeout',
+						name: 'renderJsTimeoutMs',
+						type: 'number',
+						default: 5000,
+						description: 'Timeout for JS rendering in milliseconds. Defaults to 5s.',
+					},
+					{
+						displayName: 'Total Timeout',
+						name: 'totalTimeoutMs',
+						type: 'number',
+						default: 90000,
+						description: 'Total request timeout in milliseconds. Defaults to 9s.',
+					},
+					{
+						displayName: 'Trial Timeout',
+						name: 'trialTimeoutMs',
+						type: 'number',
+						default: 30000,
+						description: 'Timeout for a single trial in milliseconds. Defaults to 30s.',
 					},
 				],
 			},
@@ -81,6 +111,10 @@ export class Scrapingfish implements INodeType {
 				const fields = this.getNodeParameter('fields', i, {}) as {
 					extractRules?: string;
 					jsScenario?: string;
+					totalTimeoutMs?: number;
+					trialTimeoutMs?: number;
+					renderJsTimeoutMs?: number;
+					headers?: string;
 				};
 				const extractRules = fields.extractRules;
 				const jsScenario = fields.jsScenario;
@@ -92,6 +126,10 @@ export class Scrapingfish implements INodeType {
 					render_js?: boolean;
 					extract_rules?: string;
 					js_scenario?: string;
+					total_timeout_ms?: number;
+					trial_timeout_ms?: number;
+					render_js_timeout_ms?: number;
+					headers?: string;
 				} = {
 					api_key: credentials.apiKey as string,
 					url: url,
@@ -107,6 +145,22 @@ export class Scrapingfish implements INodeType {
 
 				if (jsScenario) {
 					qs.js_scenario = jsScenario;
+				}
+
+				if (fields.totalTimeoutMs) {
+					qs.total_timeout_ms = fields.totalTimeoutMs;
+				}
+
+				if (fields.trialTimeoutMs) {
+					qs.trial_timeout_ms = fields.trialTimeoutMs;
+				}
+
+				if (fields.renderJsTimeoutMs) {
+					qs.render_js_timeout_ms = fields.renderJsTimeoutMs;
+				}
+
+				if (fields.headers) {
+					qs.headers = fields.headers;
 				}
 
 				const response = await this.helpers.httpRequestWithAuthentication.call(
